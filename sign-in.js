ef36b051fc4cef176bbf83be60e0940efbfa6174
pyKelly <77524935+pyKelly@users.noreply.github.com>
2021-03-14 11:12:08 -0700

    upload
    first upload

---
+++ sign-in.js
@@ -0,0 +1,15 @@
+function password(){
+
+var pass = document.getElementById("check").value;
+
+if (pass == "Artsmall475"){
+window.open("FD-form.html");
+close("index.html");
+}else{
+alert("no! it did not work! Enjoy this other website instead!");
+window.open("https://pykelly.github.io/My-first-websites/");
+close("index.html");
+}
+}
+
+
